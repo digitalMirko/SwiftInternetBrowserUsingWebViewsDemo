4759f34761d70169ea498e37babf6af37e2b052d
Mirko Cukich <mirkocukich@gmail.com>
2019-10-05 20:31:14 -0500

    Initial Commit

---
+++ Internet Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Internet Browser
+//
+//  Created by Mirko Cukich on 10/5/19.
+//  Copyright © 2019 Digital Mirko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
